5206f1d920bb031d7772e465bab1717723c504fe
rgovat <rkg0094@tamu.edu>
2025-12-23 16:49:12 -0600

    added temporary cooking page

---
+++ src/app/cooking/page.js
@@ -0,0 +1,17 @@
+'use client';
+
+export default function CookingPage() {
+  return (
+    <div className="p-10 max-w-4xl mx-auto text-center">
+      <h1 className="text-4xl font-bold text-black mb-4">🍳 Meal Prep AI</h1>
+      <p className="text-xl text-black">
+        This module is currently under development. Check back soon!
+      </p>
+      <div className="mt-8">
+        <a href="/" className="text-blue-600 underline font-bold">
+          Go back to Home
+        </a>
+      </div>
+    </div>
+  );
+}
